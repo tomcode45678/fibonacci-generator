8f3303f0b65f7503721c98b07a5ffe0a3ad03a32
thomasjohnstapleton <thomasstapleton0@gmail.com>
2015-12-24 21:57:34 +0000

    Added npm configuration code

--- fibonacci.js
+++ fibonacci.js
@@ -1,8 +1,12 @@
-var GENERATE_TO = process.argv.slice(2);
-var sequence = [0, 1];
+'use strict';
 
-for (var i = 0; i < GENERATE_TO; i++) {
-    sequence.push(sequence[0] + sequence[1]);
-    sequence.splice(0, 1);
-    console.log(sequence[1]);
+module.exports = function (generateTo) {
+    var GENERATE_TO = generateTo || process.argv.slice(2);
+    var sequence = [0, 1];
+
+    for (var i = 0; i < GENERATE_TO; i++) {
+        sequence.push(sequence[0] + sequence[1]);
+        sequence.splice(0, 1);
+        console.log(sequence[1]);
+    }
 }
