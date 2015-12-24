dfe15728bac37c2c339186228ad6d92d7cb4663f
thomasjohnstapleton <thomasstapleton0@gmail.com>
2015-12-24 22:41:58 +0000

    Removed exportation code renamed to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var GENERATE_TO = process.argv.slice(2);
+var sequence = [0, 1];
+
+for (var i = 0; i < GENERATE_TO; i++) {
+    sequence.push(sequence[0] + sequence[1]);
+    sequence.splice(0, 1);
+    console.log(sequence[1]);
+}
